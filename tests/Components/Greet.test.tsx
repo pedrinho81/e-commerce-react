4a1bb7a2a115df17760abb9b842c2df44d467775
Pedro Henrique <1pedrohenri@gmail.com>
2024-11-07 13:40:43 -0300

    feat: global vitest imports

--- tests/Components/Greet.test.tsx
+++ tests/Components/Greet.test.tsx
@@ -1,7 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { it, expect, describe } from "vitest";
 import Greet from "../../src/components/Greet";
-import "@testing-library/jest-dom/vitest";
 
 describe("Greet", () => {
   it("should render Hello with the current username when name is provided", () => {
